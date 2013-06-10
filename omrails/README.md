590280e637eba5d26258f26a14c08175ab3ec83e
Wilson JY Abraham <gmefmax@gmail.com>
2013-06-10 12:38:40 +0800

    Recreating the omrails project

--- omrails/README.md
+++
@@ -1,6 +0,0 @@
-# One Month Rails
-
- This is the sample application for
- [*One Month Rails: Teach yourself to Code*](http://www.eyidea.com)
-
- By Wilson JY Abraham
--- omrails/config/initializers/secret_token.rb
+++ omrails/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Omrails::Application.config.secret_token = 'cbeb54094c032d2bd7c50c680eae17a80f56fa953afe6ee6ea52045130f6b2024c2ff711628c03fef67824298f985680f1248ade2b463d0cff7997070fdb98b3'
+Omrails::Application.config.secret_token = '6dd212d834fb21a462c8559d9d54ebec45e085286d0759565cfad945d5d836645ad7c347cd8ce70f12602dfcedd06d6886f9dfbc8b47e7cf22fbfa1b7d7e46b6'
