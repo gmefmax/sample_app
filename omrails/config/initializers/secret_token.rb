bd38e5c638f13e520c6ab496a5af64f97d986cc8
Wilson JY Abraham <gmefmax@gmail.com>
2013-06-24 10:11:23 +0800

    Setting up dev environment on Core

--- omrails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Omrails::Application.config.secret_token = '6dd212d834fb21a462c8559d9d54ebec45e085286d0759565cfad945d5d836645ad7c347cd8ce70f12602dfcedd06d6886f9dfbc8b47e7cf22fbfa1b7d7e46b6'
